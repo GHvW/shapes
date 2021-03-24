213404c227341f71f477652618ee447f5c04bc5b
Garrett van Wageningen <ghvanw283@gmail.com>
2021-03-24 12:35:06 -0500

    add simple console app code

--- Shapeshifter.Example.Console/Program.fs
+++ Shapeshifter.Example.Console/Program.fs
@@ -1,8 +1,33 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open Parser
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Hello Shapeshifter!"
+    //let path = @"C:\Users\ghvw\projects\csharp\compass\shapefile\supercool.shp";
+    let path = @"C:\Users\ghvw\projects\rust\clay\examples\defaultish\defautish.shp";
+    
+    let bytes = ArraySegment<byte>.op_Implicit(File.ReadAllBytes(path))
+    
+    printfn "There are %A bytes in the shapefile" bytes.Count
+    
+    let (main, rest) = (mainFileHeader bytes) |> Option.get
+    printfn "main header is %A" main
+
+    let readAll (parser : Parser<'a>) : ArraySegment<byte> -> list<'a> =
+        let rec inner agg parse b =
+            match parse b with
+            | None -> agg
+            | Some (data, rest_) -> inner (data::agg) parse rest_
+
+        fun arr ->
+            inner [] parser arr
+            
+    let result = readAll (polygon |> asRecordContent |> shapefileRecord) rest
+
+    printfn "result: %A" result
+
     0 // return an integer exit code
